d6112140d3d6b3fcd0729b25e3f86007237583f0
shadi bagheri <shadibagheri80@gmail.com>
2025-02-11 22:06:17 +0330

    change banner size

--- src/components/module/Banner.js
+++ src/components/module/Banner.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 function Banner() {
     return(
-        <div className="container flex flex-col items-center w-full mx-auto">
-            <Image className="w-full h-[125px] sm:h-[170px] md:h-[220px] lg:h-[300px] xl:h-[340px]" src="/images/banner.png" width={2000} height={400} alt="banner"/>
+        <div className=" flex flex-col items-center w-full">
+            <Image className="w-full h-[125px] sm:h-[170px] md:h-[220px] lg:h-[300px] xl:h-[360px]" src="/images/banner.png" width={2000} height={400} alt="banner"/>
             <div className="w-full flex flex-col items-center my-5 sm:my-6 lg:my-10 mx-auto">
                 <h1 className="text-base sm:text-xl lg:text-[28px] font-semibold text-[#595959] tracking-wider">
                     <span className="text-[#28A745] ml-2">تورینو</span>
