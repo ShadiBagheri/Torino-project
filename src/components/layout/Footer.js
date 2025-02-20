ee99a3e460f1c95f6f4457fca240da9b58b052e1
shadi bagheri <shadibagheri80@gmail.com>
2025-02-20 12:52:41 +0330

    update

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,38 +7,38 @@ function Footer() {
             <div className="container flex flex-col lg:flex-row items-center justify-between lg:w-[1270px] mx-auto pt-8 pb-3 lg:py-5 border-t-4 border-dashed lg:border-t lg:border-solid gap-8 lg:gap-0">
                 <div className="container flex items-center justify-between w-full lg:w-[500px]">
                     <div className="flex flex-col lg:ml-20">
-                        <h1 className="text-[26px] md:text-3xl lg:text-2xl font-semibold text-[#282828] dark:text-white">تورینو</h1>
+                        <h1 className="text-2xl sm:text-[26px] md:text-3xl lg:text-2xl font-semibold text-[#282828] dark:text-white">تورینو</h1>
                         <div className="flex flex-col mt-2 lg:mt-5">
-                            <Link className="mt-2 md:mt-3 text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/about">درباره ما</Link>
-                            <Link className="mt-2 md:mt-3 text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/contact">تماس با ما</Link>
-                            <Link className="mt-2 md:mt-3 text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">چرا تورینو</Link>
-                            <Link className="mt-2 md:mt-3 text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">بیمه مسافرتی</Link>
+                            <Link className="mt-2 md:mt-3 text-lg sm:text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/about">درباره ما</Link>
+                            <Link className="mt-2 md:mt-3 text-lg sm:text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/contact">تماس با ما</Link>
+                            <Link className="mt-2 md:mt-3 text-lg sm:text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">چرا تورینو</Link>
+                            <Link className="mt-2 md:mt-3 text-lg sm:text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">بیمه مسافرتی</Link>
                         </div>
                     </div>
                     <div className="flex flex-col">
-                        <h1 className="text-[26px] md:text-3xl lg:text-2xl font-semibold text-[#282828] dark:text-white">خدمات مشتریان</h1>
+                        <h1 className="text-2xl sm:text-[26px] md:text-3xl lg:text-2xl font-semibold text-[#282828] dark:text-white">خدمات مشتریان</h1>
                         <div className="flex flex-col mt-2 lg:mt-5">
-                            <Link className="mt-2 md:mt-3 text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">پشتیبانی آنلاین</Link>
-                            <Link className="mt-2 md:mt-3 text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">راهنمای خرید</Link>
-                            <Link className="mt-2 md:mt-3 text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">راهنمای استرداد</Link>
-                            <Link className="mt-2 md:mt-3 text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">پرسش و پاسخ</Link>
+                            <Link className="mt-2 md:mt-3 text-lg sm:text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">پشتیبانی آنلاین</Link>
+                            <Link className="mt-2 md:mt-3 text-lg sm:text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">راهنمای خرید</Link>
+                            <Link className="mt-2 md:mt-3 text-lg sm:text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">راهنمای استرداد</Link>
+                            <Link className="mt-2 md:mt-3 text-lg sm:text-xl md:text-2xl lg:text-lg font-normal text-[#282828] dark:text-white" href="/">پرسش و پاسخ</Link>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-row-reverse lg:flex-col justify-between lg:items-end w-full md:my-4 lg:my-0">
                     <div className="flex flex-col mt-10 items-end px-2 md:p-0">
-                        <Image className="w-[130px] h-[36px] md:w-[135px] md:h-[40px] lg:w-[146px] lg:h-[44px]" src="/images/torino.png" width={146} height={44} alt="torino-logo"/>
-                        <h4 className="mt-5 md:mt-6 mb-8 text-lg md:text-xl font-normal dark:text-white">تلفن پشتیبانی: 8574-021</h4>
+                        <Image className="w-[110px] h-[30px] sm:w-[130px] sm:h-[36px] md:w-[135px] md:h-[40px] lg:w-[146px] lg:h-[44px]" src="/images/torino.png" width={146} height={44} alt="torino-logo"/>
+                        <h4 className="mt-5 md:mt-6 mb-8 text-sm sm:text-lg md:text-xl font-normal dark:text-white">تلفن پشتیبانی: 8574-021</h4>
                     </div>
                     <div className="flex flex-col-reverse items-center lg:flex-row gap-5 md:gap-10 lg:gap-10">
                         <div className="flex gap-5 md:gap-10 lg:gap-10">
-                            <Image className="w-[60px] h-[65px] md:w-[65px] md:h-[70px] lg:w-[78px] lg:h-[74px]" src="/images/airline.png" width={78} height={74} alt="airline-logo"/>
-                            <Image className="w-[60px] h-[65px] md:w-[65px] md:h-[70px] lg:w-[71px] lg:h-[74px]" src="/images/passenger.png" width={71} height={74} alt="passenger-logo"/>
+                            <Image className="w-[30px] h-[35px] sm:w-[60px] sm:h-[65px] md:w-[65px] md:h-[70px] lg:w-[78px] lg:h-[74px]" src="/images/airline.png" width={78} height={74} alt="airline-logo"/>
+                            <Image className="w-[30px] h-[35px] sm:w-[60px] sm:h-[65px] md:w-[65px] md:h-[70px] lg:w-[71px] lg:h-[74px]" src="/images/passenger.png" width={71} height={74} alt="passenger-logo"/>
                         </div>
                         <div className="flex gap-5 md:gap-10 lg:gap-10">
-                            <Image className="w-[60px] h-[65px] md:w-[65px] md:h-[70px] lg:w-[71px] lg:h-[74px]" src="/images/qrcode.png" width={71} height={74} alt="qrcode-logo"/>
-                            <Image className="w-[60px] h-[65px] md:w-[65px] md:h-[70px] lg:w-[71px] lg:h-[74px]" src="/images/samandehi.png" width={71} height={74} alt="samandehi-logo"/>
-                            <Image className="w-[60px] h-[65px] md:w-[65px] md:h-[70px] lg:w-[71px] lg:h-[74px]" src="/images/aria.png" width={71} height={74} alt="aria-logo"/>
+                            <Image className="w-[30px] h-[35px] sm:w-[60px] sm:h-[65px] md:w-[65px] md:h-[70px] lg:w-[71px] lg:h-[74px]" src="/images/qrcode.png" width={71} height={74} alt="qrcode-logo"/>
+                            <Image className="w-[30px] h-[35px] sm:w-[60px] sm:h-[65px] md:w-[65px] md:h-[70px] lg:w-[71px] lg:h-[74px]" src="/images/samandehi.png" width={71} height={74} alt="samandehi-logo"/>
+                            <Image className="w-[30px] h-[35px] sm:w-[60px] sm:h-[65px] md:w-[65px] md:h-[70px] lg:w-[71px] lg:h-[74px]" src="/images/aria.png" width={71} height={74} alt="aria-logo"/>
                         </div>
                     </div>
                 </div>
