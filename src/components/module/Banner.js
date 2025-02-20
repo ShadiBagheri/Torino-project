c462a6af9e35b7525e2efabbf6a6569b3244d0f1
shadi bagheri <shadibagheri80@gmail.com>
2025-02-20 12:45:16 +0330

    update

--- src/components/module/Banner.js
+++ src/components/module/Banner.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 function Banner() {
     return(
         <div className="flex flex-col items-center w-full">
-            <Image className="w-full h-[125px] sm:h-[170px] md:h-[220px] lg:h-[300px] xl:h-[360px]" src="/images/banner.png" width={2000} height={400} alt="banner"/>
+            <Image className="w-full h-[180px] sm:h-[260px] md:h-[300px] lg:h-[330px] xl:h-[390px]" src="/images/banner.png" width={2000} height={400} alt="banner"/>
             <div className="w-full flex flex-col items-center my-5 sm:my-6 lg:my-10 mx-auto">
                 <h1 className="text-base sm:text-xl lg:text-[28px] font-semibold text-[#595959] tracking-wider">
                     <span className="text-[#28A745] ml-2">تورینو</span>
