31bca3bbdf53dd92bea4919b9a6ef329f8d9bb97
shadi bagheri <shadibagheri80@gmail.com>
2025-02-25 12:55:28 +0330

    responsive dropdown

--- src/components/module/NavButton.js
+++ src/components/module/NavButton.js
@@ -20,15 +20,15 @@ function NavButton() {
     const { data } = useGetUserData();
 
     if (data?.data) return(
-        <div className="container flex items-center justify-between w-[180px] h-[44px] p-4">
-            <Image src="/images/profile.png" width={20} height={20} alt="login-icon"/>
-            <div className="flex items-center w-[108px] h-[28px] mt-2 mr-2">
-                <p className="text-center text-lg font-medium text-[#28A745]">
+        <div className="container flex items-center justify-between w-[125px] h-[35px] sm:w-[140px] md:w-[180px] h-[44px] p-2 md:p-4">
+            <Image src="/images/profile.png" className="w-[14px] h-[14px] sm:w-[16px] sm:h-[16px] md:w-[20px] md:h-[20px]" width={20} height={20} alt="login-icon"/>
+            <div className="flex items-center w-[108px] h-[28px] mt-2 mr-1 md:mr-2">
+                <p className="text-center text-sm sm:text-base md:text-lg font-medium text-[#28A745]">
                     {data?.data.mobile}
                 </p>
             </div>
             {dropDown && <DropDown mobile={data?.data.mobile}/>}
-            <SlArrowDown onClick={dropDownHandler} className="mt-1 text-[#28A745] size-4 cursor-pointer"/>
+            <SlArrowDown onClick={dropDownHandler} className="mt-1  text-[#28A745] size-3 md:size-4 cursor-pointer"/>
         </div>
     )
 }
