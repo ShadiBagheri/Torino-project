c43c0bd67391e2c5ad65753ca1bde737f2402654
shadi bagheri <shadibagheri80@gmail.com>
2025-02-25 13:07:44 +0330

    update signup button

--- src/components/module/SignupBtn.js
+++ src/components/module/SignupBtn.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 function SignupBtn({ modalHandler }) {
     return(
         <>
-            <button onClick={modalHandler} className="container hidden lg:flex items-center justify-between w-[166px] h-[44px] p-2 border-2 border-solid border-[#28A745] rounded-[8px]">
+            <button onClick={modalHandler} className="container hidden lg:flex items-center justify-between w-[166px] h-[44px] mt-5 p-2 border-2 border-solid border-[#28A745] rounded-[8px]">
                 <Image className="mr-2" src="/images/profile.png" width={20} height={20} alt="login-icon"/>
                 <div className="flex w-[108px] h-[28px]">
                     <p className="text-center text-lg font-medium text-[#28A745]">
--- src/components/module/SignupBtnSm.js
+++ src/components/module/SignupBtnSm.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 function SignupBtnSm({ modalHandler }) {
     return(
         <button onClick={modalHandler}>
-            <Image className="flex lg:hidden w-[35px] h-[35px] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] z-10" src="/images/signin.png" width={40} height={40} alt="signin"/>
+            <Image className="flex lg:hidden w-[26px] h-[26px] sm:w-[36px] sm:h-[36px] md:w-[40px] md:h-[40px] mt-3 md:mt-3.5 z-10" src="/images/signin.png" width={40} height={40} alt="signin"/>
         </button>
     )
 }
